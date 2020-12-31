b19214c1fad4ccaaac2a4a2f2c21a2bc90238700
zyc <zyc2010303268@163.com>
2020-12-31 10:55:30 +0800

    Minimum Swap To Make Strings Equal

---
+++ BUILD
@@ -0,0 +1,6 @@
+cc_binary(
+  name = 'minimum_change_num',
+  srcs = ['MinimumSwapToMakeStringSEqual.cc'],
+)
+
+
---
+++ MinimumSwapToMakeStringSEqual.cc
@@ -0,0 +1,60 @@
+// Author zyc2010303268@163.com(zyc)
+// description:
+// https://leetcode.com/problems/minimum-swaps-to-make-strings-equal
+// there are (x,x), (x,y), (y,x) (y,y) pairs
+// (x,x) and (y,y) needn't operation
+// one swap can make (x,y) and (x,y) to the same string, which also apply to
+// (y,x) and (y,x) pair, while two swaps can make (x,y) and (y,x) to the same
+// string
+#include <iostream>
+using namespace std;
+int firstNotMatchIndex(const string &s1, const string &s2, char letter,
+                       int index) {
+  for (int i = index; i < s2.size(); ++i) {
+    if (s1[i] == letter && s2[i] != letter) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void swap(char &c1, char &c2) {
+  char tmp = c1;
+  c1 = c2;
+  c2 = tmp;
+}
+
+int minimumSwap(string s1, string s2) {
+  if (s1.size() != s2.size()) {
+    return -1;
+  }
+  int minimum = 0;
+  for (int i = 0; i < s1.size(); ++i) {
+    if (s1[i] == s2[i]) {
+      continue;
+    }
+    int index = firstNotMatchIndex(s1, s2, s1[i], i + 1);
+    if (index == -1) {
+      if (i == s1.size() - 1) {
+        return -1;
+      }
+      swap(s1[i], s2[i]);
+      ++minimum;
+      int sub = minimumSwap(s1, s2);
+      if (sub == -1) {
+        return -1;
+      }
+      return minimum + sub;
+    }
+    swap(s1[i], s2[index]);
+    ++minimum;
+  }
+  return minimum;
+}
+
+int main() {
+  string s1, s2;
+  cin >> s1 >> s2;
+  cout << "最小次数：" << minimumSwap(s1, s2);
+  return 0;
+}
